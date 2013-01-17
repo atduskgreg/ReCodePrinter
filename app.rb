11f15d5eecd2a5230c1834cb571c6c80c0c06ac5
Greg Borenstein <greg.borenstein@gmail.com>
2013-01-17 02:37:53 -0500

    check for delivery count param correctly

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ end
 
 get '/edition/' do
 	delivery = 2
-	if params.keys.include?(:delivery_count)
+	if params[:delivery_count]
 		delivery = params[:delivery_count].to_i
 	end
 
@@ -29,7 +29,7 @@ get '/all' do
 	erb :all
 end
 
-post "/validate_config" do
+post "/validate_config/" do
   if ["original", "recode"].include? params["edition_type"]
   	"{\"valid\" : true}"
   else
