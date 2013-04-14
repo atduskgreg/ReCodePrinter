b3bab8d439ae1a8fc42f3ef4de385cfa4f58910e
Greg Borenstein <greg.borenstein@gmail.com>
2013-04-14 18:31:55 -0400

    add front page

--- app.rb
+++ app.rb
@@ -3,36 +3,37 @@ require 'bundler/setup'
 require 'sinatra'
 require './models'
 
-get '/' do
-	"hello"
-end
-
-get '/edition/' do
-	delivery = 2
-	if params[:delivery_count]
-		delivery = params[:delivery_count].to_i
+	get '/' do
+		erb :index
 	end
 
-	@piece = Piece.first :delivery_order => delivery
-	etag @piece.delivery_order
-	erb :edition
-end
+	get '/edition/' do
+		delivery = 2
+		if params[:delivery_count]
+			delivery = params[:delivery_count].to_i
+		end
+	
+		@piece = Piece.first :delivery_order => delivery
+		etag @piece.delivery_order
+		erb :edition
+	end
+	
+	get '/sample/' do
+		@piece = Piece.first :delivery_order => 10
+		etag "sample"+@piece.delivery_order
+		erb :edition
+	end
+	
+	get '/all' do
+		@pieces = Piece.all
+		erb :all
+	end
+	
+	post "/validate_config/" do
+	  if ["original", "recode"].include? params["edition_type"]
+	  	"{\"valid\" : true}"
+	  else
+	  	"{\"valid\": false, \"errors\": [\"Please select a ReCodePrinter edition.\"]}"
+	  end
+	end
 
-get '/sample/' do
-	@piece = Piece.first :delivery_order => 10
-	etag "sample"+@piece.delivery_order
-	erb :edition
-end
-
-get '/all' do
-	@pieces = Piece.all
-	erb :all
-end
-
-post "/validate_config/" do
-  if ["original", "recode"].include? params["edition_type"]
-  	"{\"valid\" : true}"
-  else
-  	"{\"valid\": false, \"errors\": [\"Please select a ReCodePrinter edition.\"]}"
-  end
-end
